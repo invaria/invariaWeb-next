b1137997de96b9901f656f9a28903004686f34c5
wesleytw <77147076+wesleytw@users.noreply.github.com>
2022-08-03 18:56:40 +0800

    kyc

--- pages/api/callback copy.js
+++ pages/api/callback copy.js
@@ -1,18 +1,18 @@
-// const sgMail = require('@sendgrid/mail')
-// console.log("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")
-// sgMail.setApiKey("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")
-// const msg = {
-//   to: 'bill411493@gmail.com',
-//   from: 'wesliutw@gmail.com',
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '',
-// }
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
+const sgMail = require('@sendgrid/mail')
+console.log("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")
+sgMail.setApiKey("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") //kyc2
+const msg = {
+  to: 'bill411493@gmail.com',
+  from: 'wesliutw@gmail.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js yoyytotoototo</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
