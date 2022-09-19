4521e81ad3b51183e5ad26d3d2b5d7730f9115c9
wesleytw <wesliutw@gmail.com>
2022-09-19 09:27:23 +0800

    comolete filt

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -18,6 +18,7 @@ const Dashboard = () => {
   const [showtog, setshowtog] = useState("All")
   const [starttime, setstarttime] = useState()
   const [endtime, setendtime] = useState()
+  const [fmenu, setfmenu] = useState(false)
 
   async function getdata() {
     const state = await getUser(address)
@@ -119,6 +120,8 @@ const Dashboard = () => {
     let endDate = (new Date(endyear, endmonth - 1, endday)).getTime()
     setendtime(endDate)
     // console.log(typeof inputs.selectStartDate, startday, startmonth, startyear, startDate)
+    setfmenu(false)
+
   }
 
   function reset() {
@@ -154,7 +157,12 @@ const Dashboard = () => {
         </div>
         {(tabState == "activity") &&
           <div className="relative min-h-[70vw] w-full border-t border-invar-main-purple">
-            <form className="flex justify-between my-9 mx-[30px] sm:mx-[30px] md:mx-[130px] lg:mx-[230px] max-w-full z-10 " onSubmit={handleSubmit} name="form">
+            <div className=" md:hidden max-w-full mt-4 h-10 bg-invar-main-purple rounded flex justify-center items-center text-sm font mx-[30px] sm:mx-[30px] md:mx-[130px] lg:mx-[230px] "
+              onClick={() => setfmenu(!fmenu)}>
+              <img src="/icons/filt.png" alt="" className=" w-4 h-4 mr-2" />
+              Filter
+            </div>
+            <form className=" hidden md:flex justify-between my-9 mx-[30px] sm:mx-[30px] md:mx-[130px] lg:mx-[230px] max-w-full z-10 " onSubmit={handleSubmit} name="form">
               <div className=' flex'>
                 <label className="w-[238px] mb-6 block mr-4">
                   <div className="relative flex bg-invar-main-purple rounded items-center">
@@ -263,6 +271,84 @@ const Dashboard = () => {
             </div>
           </div>
         }
+
+        {fmenu &&
+          <form
+            onSubmit={handleSubmit} name="form"
+            className=" fixed top-[60px] z-30 w-full h-screen py-[24px] px-[16px] flex flex-col justify-start items-start md:hidden text-white bg-gradient-to-b from-primary to-[#1E1722]"
+          >
+            <div className="flex justify-between w-full">
+              <h1 className="font-semibold text-xl mb-7 cursor-pointer">Filters</h1>
+              <div
+                onClick={() => setfmenu(!fmenu)}
+                className="btn btn-sm p-0 bg-transparent border-none hover:bg-transparent">
+                <img className="h-[20px] w-[20px]" src='/icons/ic_close.svg' alt="" />
+              </div>
+            </div>
+            <label className="w-full mb-6 block mr-4">
+              <p className=" text-invar-light-grey font-normal text-sm">Item</p>
+              <div className=" relative flex bg-invar-main-purple rounded items-center">
+                <select name="selectIDtype" onChange={handleChange} value={inputs.selectIDtype || ""}
+                  required className="appearance-none block bg-invar-main-purple w-full h-10 rounded 
+                     cursor-pointer focus:outline-none text-white font-normal pl-[15px] pr-[40px] text-start">
+                  <option value="Amwaj20">Amwaj20</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                </div>
+              </div>
+            </label>
+            <label className="w-full mb-6 block mr-4">
+              <p className=" mb-1 text-invar-light-grey font-normal text-sm">Type</p>
+              <div className="relative flex bg-invar-main-purple rounded items-center">
+                <select name="selectType" onChange={handleChange} value={inputs.selectType || ""}
+                  required className="appearance-none block bg-invar-main-purple w-full h-10 rounded 
+                     cursor-pointer focus:outline-none text-white font-normal pl-[15px] pr-[40px] text-start">
+                  <option value="All">All</option>
+                  <option value="Pre-Sale">Pre-Sale</option>
+                  <option value="Whitelist">Whitelist</option>
+                  <option value="Public Sale">Public Sale</option>
+                  <option value="Unstake">Unstake</option>
+                  <option value="Claim">Claim</option>
+                  <option value="Redemption">Redemption</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                </div>
+              </div>
+            </label>
+            <p className=" mb-1 text-invar-light-grey font-normal text-sm">Date</p>
+            <label className=" w-full mb-6 flex bg-invar-main-purple">
+              <div className="relative flex bg-invar-main-purple rounded items-center">
+                <input
+                  name="selectStartDate" type="date" onChange={handleChange} value={inputs.selectStartDate || ""}
+                  required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] appearance-none"
+                />
+                <div className=" pointer-events-none absolute inset-y-0 right-[5px] flex items-center  text-white">
+                  <img className=" w-4 h-4 cursor-pointer" src="/icons/ic_calendar.png" alt="" />
+                </div>
+              </div>
+              <div className="relative flex bg-invar-main-purple rounded items-center">
+                <img className=" mx-2 w-4 h-4 " src="/icons/ic_right.svg" alt="" />
+                <input
+                  name="selectEndDate" type="date" onChange={handleChange} value={inputs.selectEndDate || ""}
+                  required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] appearance-none"
+                />
+                <div className=" pointer-events-none absolute inset-y-0 right-[5px] flex items-center  text-white">
+                  <img className=" w-4 h-4 cursor-pointer mr-2" src="/icons/ic_calendar.png" alt="" />
+                </div>
+              </div>
+            </label>
+            <div className="flex absolute bottom-20">
+              <input
+                type="submit" value="Search"
+                className=" w-full btn btn-sm inline-block bg-invar-dark hover:bg-invar-main-purple rounded text-white px-6 py-2 h-10 normal-case text-sm font-semibold cursor-pointer border-none"
+              />
+              <div className=' flex-grow w-full btn btn-sm inline-block bg-transparent hover:bg-invar-main-purple rounded text-invar-light-grey ml-2 -mr-2 px-6 py-2 h-10 normal-case text-sm font-semibold cursor-pointer border-invar-dark ' onClick={() => reset()}>Reset</div>
+            </div>
+          </form>
+        }
+        <div className=" h-96 md:hidden"></div>
         <Footer />
       </div>
     </div>
