eaa67ff45c5af4c700ea0e673602184d0216a30f
wesleytw <77147076+wesleytw@users.noreply.github.com>
2022-10-12 19:54:40 +0800

    fix appear none

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -326,7 +326,7 @@ const Dashboard = () => {
               <div className="relative flex bg-invar-main-purple rounded items-center">
                 <input
                   name="selectStartDate" type="date" onChange={handleChange} value={inputs.selectStartDate || "mm/dd/yyyy"}
-                  required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] appearance-none"
+                  required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] "
                 />
                 <div className=" pointer-events-none absolute inset-y-0 right-[5px] flex items-center  text-white">
                   <img className=" w-4 h-4 cursor-pointer" src="/icons/ic_calendar.png" alt="" />
@@ -336,7 +336,7 @@ const Dashboard = () => {
                 <img className=" mx-2 w-4 h-4 " src="/icons/ic_right.svg" alt="" />
                 <input
                   name="selectEndDate" type="date" onChange={handleChange} value={inputs.selectEndDate || "mm/dd/yyyy"}
-                  required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] appearance-none"
+                  required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] "
                 />
                 <div className=" pointer-events-none absolute inset-y-0 right-[5px] flex items-center  text-white">
                   <img className=" w-4 h-4 cursor-pointer mr-2" src="/icons/ic_calendar.png" alt="" />
