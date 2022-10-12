0d12279e1e801d4d80bf4292eaa7d40fc16a0859
wesleytw <77147076+wesleytw@users.noreply.github.com>
2022-10-12 20:38:30 +0800

    test

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -694,6 +694,7 @@ const Dashboard = () => {
                   name="selectStartDate" type="date" onChange={handleChange} value={inputs.selectStartDate || ""}
                   required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] appearance-none"
                 />
+                {!inputs.selectStartDate && <p className=' -ml-10 pr-8 md:hidden'>mm/dd/yyyy</p>}
                 <div className=" pointer-events-none absolute inset-y-0 right-[5px] flex items-center  text-white">
                   <img className=" w-4 h-4 cursor-pointer" src="/icons/ic_calendar.png" alt="" />
                 </div>
@@ -704,6 +705,7 @@ const Dashboard = () => {
                   name="selectEndDate" type="date" onChange={handleChange} value={inputs.selectEndDate || ""}
                   required className="block bg-invar-main-purple w-full h-10 rounded  outline-none text-white font-normal pl-[15px] appearance-none"
                 />
+                {!inputs.selectEndDate && <p className=' -ml-10 pr-8 md:hidden'>mm/dd/yy</p>}
                 <div className=" pointer-events-none absolute inset-y-0 right-[5px] flex items-center  text-white">
                   <img className=" w-4 h-4 cursor-pointer mr-2" src="/icons/ic_calendar.png" alt="" />
                 </div>
