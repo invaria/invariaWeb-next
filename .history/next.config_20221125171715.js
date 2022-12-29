617d07de5bb0423a1bd7a97444e0aaef133e0add
invar-shawn <shawn.tseng@pivoterra.co>
2022-12-29 17:20:31 +0800

    drag animation

--- .history/next.config_20221125171715.js
+++ .history/next.config_20221125171715.js
@@ -2,7 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   compress: false,
   i18n
 }
