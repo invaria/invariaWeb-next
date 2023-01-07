924a1f1c74335aeda0c8626f76bf927cf79d1ae9
shawn <shawn.tseng@pivoterra.co>
2023-01-07 15:26:56 +0800

    switch to mainnet

--- .history/pages/_app_20230107152547.jsx
+++ .history/pages/_app_20230107152547.jsx
@@ -2,16 +2,19 @@ import "../styles/globals.css";
 import reportWebVitals from "../src/reportWebVitals";
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 import { appWithTranslation } from "next-i18next";
+import { MusicProvider } from "../context/music-context"
 
-//export const desiredChainId = ChainId.Mainnet
-export const desiredChainId = ChainId.Goerli;
+export const desiredChainId = ChainId.Mainnet
+//export const desiredChainId = ChainId.Goerli;
 
 // export const AppContext = createContext();
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider desiredChainId={desiredChainId}>
-      <Component {...pageProps} />
+      <MusicProvider>
+        <Component {...pageProps} />
+      </MusicProvider>
     </ThirdwebProvider>
   );
 }
