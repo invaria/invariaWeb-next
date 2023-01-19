bbcbd0c00a908c98f5cd0cecac2137274d56383f
invar-shawn <114984891+invar-shawn@users.noreply.github.com>
2023-01-19 23:57:22 +0800

    change chainid

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,8 +4,8 @@ import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 import { appWithTranslation } from "next-i18next";
 import { MusicProvider } from "../context/music-context"
 
-//export const desiredChainId = ChainId.Mainnet
-export const desiredChainId = ChainId.Goerli;
+export const desiredChainId = ChainId.Mainnet
+//export const desiredChainId = ChainId.Goerli;
 
 // export const AppContext = createContext();
 
